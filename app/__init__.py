f8f3ff72812470ce520ca10c9c9997ab866e2b01
Guillaume Bolivard <gbolivard@luc.edu>
2026-01-22 18:37:10 -0400

    Créer fichier __init__.py pour initialiser le package app

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+"""TRAMIL-API package."""
+
+__version__ = "0.1.0"
+__author__ = "Guillaume Bolivard"
+__license__ = "MIT"
+__description__ = "REST API for TRAMIL Caribbean medicinal plants database"
+
+from .main import app
+
+__all__ = ["app"]
